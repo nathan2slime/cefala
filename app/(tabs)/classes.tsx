b0bbfb153ec9699e144ddfe804be076ad686cdf4
Samuel Sanderson <sandersonsamuel401@gmail.com>
2025-09-14 22:18:59 -0300

    feat: add date formatting to classes screen and update dependencies

--- app/(tabs)/classes.tsx
+++ app/(tabs)/classes.tsx
@@ -1,17 +1,41 @@
 import { AuthContext } from "@/components/auth-provider";
 import { Page } from "@/components/page";
-import { supabase } from "@/supabase.config";
+import { format } from "date-fns";
 import { useContext } from "react";
 import { StyleSheet } from "react-native";
-import { Button, Text } from "react-native-paper";
+import { Card, Text } from "react-native-paper";
+import { ptBR } from "date-fns/locale";
 
 export default function ClassesScreen() {
   const { session } = useContext(AuthContext);
 
+  const classes = [
+    {
+      id: 1,
+      title: "Turma 01",
+      createdAt: new Date(),
+    },
+  ];
+
   return (
     <Page>
-      <Button mode="contained" onPress={() => supabase.auth.signOut()}>Classes Screen</Button>
-      <Text variant="bodyMedium">Welcome, {session?.user.email}</Text>
+      {/* <Button mode="contained" onPress={() => supabase.auth.signOut()}>
+        Classes Screen
+      </Button> */}
+      <Text variant="bodyMedium">
+        Welcome, {session?.user.user_metadata.name}
+      </Text>
+
+      {classes.map((classItem) => (
+        <Card style={{borderRadius: 8}}>
+          <Card.Content>
+            <Text variant="titleMedium">{classItem.title}</Text>
+            <Text variant="bodyMedium">
+              {format(classItem.createdAt, "EEEE, dd 'de' MMMM 'de' yyyy", { locale: ptBR })}
+            </Text>
+          </Card.Content>
+        </Card>
+      ))}
     </Page>
   );
 }
